27cf749178898fe2edc079b69fc3c50ae9a1b496
mrproper1337 <properxxxxx@gmail.com>
2014-11-13 13:07:15 +0200

    UI,TM fix

--- src/MainForm.java
+++ src/MainForm.java
@@ -17,17 +17,28 @@ import javax.swing.table.DefaultTableModel;
  */
 public class MainForm extends JFrame {
 
-    private ConnectHibernate ch;
     private DefaultTableModel students_tm,groups_tm;
-    //private List groupList,studentList;
-
+    ConnectHibernate ch;
     public MainForm() {
         ch = new ConnectHibernate();
         initComponents();
-        setModels(ch.loadTable("from Group"),ch.loadTable("from Student"));
-        table1.setModel(students_tm);
+        comboBox2.addItem("Хлопці");
+        comboBox2.addItem("Дівчата");
+        setModels();
+        table1.setModel(groups_tm);
+
     }
-    private void setModels(List groupList,List studentList){
+    private void setModels(){
+        int check=0;
+        if(checkBox1.isSelected())check=1;
+        else check=0;
+        System.out.println("from Student where( gender="
+                +(comboBox2.getSelectedIndex())
+                +" and healthGroup="+check+")");
+        final List groupList=ch.loadTable("from Group"),
+                studentList=ch.loadTable("from Student where( gender="
+                +(comboBox2.getSelectedIndex())
+                +" and healthGroup="+check+")");
         groups_tm = new DefaultTableModel(){
 
             @Override
@@ -147,7 +158,7 @@ public class MainForm extends JFrame {
                     case 1:
                         return res.getName();
                     case 2:
-                        if(res.getGender()==1)
+                        if(res.getGender()==0)
                             return "хлопець";
                         else
                             return "дівчина";
@@ -166,17 +177,12 @@ public class MainForm extends JFrame {
         comboBox1.addItem("Всі");
         for(Group a:(List<pojo.Group>)groupList)
             comboBox1.addItem(a.getGroupName());
-
-        comboBox2.addItem("Хлопці");
-        comboBox2.addItem("Дівчата");
-
     }
 
-    private void comboBox1ItemStateChanged(ItemEvent e) {
-        if(comboBox1.getSelectedIndex()!=0){
-            comboBox2.setEnabled(true);
-        }
-        else comboBox2.setEnabled(false);
+    private void filterChange(ActionEvent e) {
+        setModels();
+        if(comboBox1.getSelectedIndex()==0)table1.setModel(groups_tm);
+        else table1.setModel(students_tm);
     }
 
     private void initComponents() {
@@ -184,7 +190,7 @@ public class MainForm extends JFrame {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - Ash Hisenberg
         tabbedPane1 = new JTabbedPane();
-        ReadForm = new JPanel();
+        panel1 = new JPanel();
         scrollPane1 = new JScrollPane();
         table1 = new JTable();
         panel3 = new JPanel();
@@ -192,7 +198,7 @@ public class MainForm extends JFrame {
         comboBox2 = new JComboBox();
         checkBox1 = new JCheckBox();
         comboBox3 = new JComboBox();
-        WriteForm = new JPanel();
+        panel2 = new JPanel();
 
         //======== this ========
         Container contentPane = getContentPane();
@@ -200,15 +206,15 @@ public class MainForm extends JFrame {
         //======== tabbedPane1 ========
         {
 
-            //======== ReadForm ========
+            //======== panel1 ========
             {
 
                 // JFormDesigner evaluation mark
-                ReadForm.setBorder(new javax.swing.border.CompoundBorder(
+                panel1.setBorder(new javax.swing.border.CompoundBorder(
                     new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
                         "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
                         javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                        java.awt.Color.red), ReadForm.getBorder())); ReadForm.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+                        java.awt.Color.red), panel1.getBorder())); panel1.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
 
 
                 //======== scrollPane1 ========
@@ -224,85 +230,98 @@ public class MainForm extends JFrame {
                     panel3.setLayout(panel3Layout);
                     panel3Layout.setHorizontalGroup(
                         panel3Layout.createParallelGroup()
-                            .addGap(0, 443, Short.MAX_VALUE)
+                            .addGap(0, 487, Short.MAX_VALUE)
                     );
                     panel3Layout.setVerticalGroup(
                         panel3Layout.createParallelGroup()
-                                .addGap(0, 500, Short.MAX_VALUE)
+                            .addGap(0, 0, Short.MAX_VALUE)
                     );
                 }
 
                 //---- comboBox1 ----
-                comboBox1.addItemListener(new ItemListener() {
+                comboBox1.addActionListener(new ActionListener() {
                     @Override
-                    public void itemStateChanged(ItemEvent e) {
-                        comboBox1ItemStateChanged(e);
+                    public void actionPerformed(ActionEvent e) {
+                        filterChange(e);
                     }
                 });
 
                 //---- comboBox2 ----
-                comboBox2.setEnabled(false);
+                comboBox2.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        filterChange(e);
+                    }
+                });
 
                 //---- checkBox1 ----
                 checkBox1.setText("\u0421\u043f\u0435\u0446\u0433\u0440\u0443\u043f\u0430");
-                checkBox1.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+                checkBox1.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+                checkBox1.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        filterChange(e);
+                    }
+                });
 
-                GroupLayout ReadFormLayout = new GroupLayout(ReadForm);
-                ReadForm.setLayout(ReadFormLayout);
-                ReadFormLayout.setHorizontalGroup(
-                    ReadFormLayout.createParallelGroup()
-                            .addGroup(ReadFormLayout.createSequentialGroup()
-                                    .addContainerGap()
-                                    .addGroup(ReadFormLayout.createParallelGroup()
-                                            .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 595, GroupLayout.PREFERRED_SIZE)
-                                            .addGroup(ReadFormLayout.createSequentialGroup()
-                                                    .addComponent(comboBox1, GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE)
-                                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                                    .addComponent(comboBox2, GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE)
-                                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                                    .addComponent(checkBox1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                                    .addGap(32, 32, 32)))
-                                    .addGroup(ReadFormLayout.createParallelGroup()
-                                            .addComponent(panel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                            .addGroup(ReadFormLayout.createSequentialGroup()
-                                                    .addComponent(comboBox3, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
-                                                    .addGap(0, 193, Short.MAX_VALUE)))
-                                    .addContainerGap())
+                GroupLayout panel1Layout = new GroupLayout(panel1);
+                panel1.setLayout(panel1Layout);
+                panel1Layout.setHorizontalGroup(
+                    panel1Layout.createParallelGroup()
+                        .addGroup(panel1Layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addGroup(panel1Layout.createParallelGroup()
+                                .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addGroup(panel1Layout.createSequentialGroup()
+                                    .addComponent(comboBox1, GroupLayout.PREFERRED_SIZE, 173, GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(comboBox2, GroupLayout.PREFERRED_SIZE, 173, GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(checkBox1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addGroup(panel1Layout.createParallelGroup()
+                                .addComponent(panel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(panel1Layout.createSequentialGroup()
+                                    .addComponent(comboBox3, GroupLayout.PREFERRED_SIZE, 173, GroupLayout.PREFERRED_SIZE)
+                                    .addGap(0, 314, Short.MAX_VALUE)))
+                            .addContainerGap())
                 );
-                ReadFormLayout.setVerticalGroup(
-                    ReadFormLayout.createParallelGroup()
-                            .addGroup(ReadFormLayout.createSequentialGroup()
-                                    .addContainerGap()
-                                    .addGroup(ReadFormLayout.createParallelGroup()
-                                            .addComponent(panel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                            .addComponent(scrollPane1, GroupLayout.DEFAULT_SIZE, 500, Short.MAX_VALUE))
-                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
-                                    .addGroup(ReadFormLayout.createParallelGroup()
-                                            .addComponent(comboBox1, GroupLayout.Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                                            .addComponent(comboBox2, GroupLayout.Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE)
-                                            .addComponent(checkBox1, GroupLayout.Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                            .addComponent(comboBox3, GroupLayout.Alignment.TRAILING))
-                                    .addContainerGap())
+                panel1Layout.setVerticalGroup(
+                    panel1Layout.createParallelGroup()
+                        .addGroup(panel1Layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addGroup(panel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                .addComponent(scrollPane1)
+                                .addComponent(panel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addGroup(panel1Layout.createParallelGroup()
+                                .addComponent(comboBox1)
+                                .addComponent(checkBox1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
+                                    .addGap(0, 0, Short.MAX_VALUE)
+                                    .addGroup(panel1Layout.createParallelGroup()
+                                        .addComponent(comboBox3, GroupLayout.Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(comboBox2, GroupLayout.Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE))))
+                            .addContainerGap())
                 );
-                ReadFormLayout.linkSize(SwingConstants.VERTICAL, new Component[] {checkBox1, comboBox1, comboBox2, comboBox3});
             }
-            tabbedPane1.addTab("\u041f\u0435\u0440\u0435\u0433\u043b\u044f\u0434", ReadForm);
+            tabbedPane1.addTab("\u041f\u0435\u0440\u0435\u0433\u043b\u044f\u0434", panel1);
 
-            //======== WriteForm ========
+            //======== panel2 ========
             {
 
-                GroupLayout WriteFormLayout = new GroupLayout(WriteForm);
-                WriteForm.setLayout(WriteFormLayout);
-                WriteFormLayout.setHorizontalGroup(
-                    WriteFormLayout.createParallelGroup()
-                        .addGap(0, 1052, Short.MAX_VALUE)
+                GroupLayout panel2Layout = new GroupLayout(panel2);
+                panel2.setLayout(panel2Layout);
+                panel2Layout.setHorizontalGroup(
+                    panel2Layout.createParallelGroup()
+                        .addGap(0, 957, Short.MAX_VALUE)
                 );
-                WriteFormLayout.setVerticalGroup(
-                    WriteFormLayout.createParallelGroup()
-                        .addGap(0, 563, Short.MAX_VALUE)
+                panel2Layout.setVerticalGroup(
+                    panel2Layout.createParallelGroup()
+                        .addGap(0, 468, Short.MAX_VALUE)
                 );
             }
-            tabbedPane1.addTab("\u0420\u0435\u0434\u0430\u0433\u0443\u0432\u0430\u043d\u043d\u044f", WriteForm);
+            tabbedPane1.addTab("\u0420\u0435\u0434\u0430\u0433\u0443\u0432\u0430\u043d\u043d\u044f", panel2);
         }
 
         GroupLayout contentPaneLayout = new GroupLayout(contentPane);
@@ -323,7 +342,7 @@ public class MainForm extends JFrame {
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - Ash Hisenberg
     private JTabbedPane tabbedPane1;
-    private JPanel ReadForm;
+    private JPanel panel1;
     private JScrollPane scrollPane1;
     private JTable table1;
     private JPanel panel3;
@@ -331,6 +350,6 @@ public class MainForm extends JFrame {
     private JComboBox comboBox2;
     private JCheckBox checkBox1;
     private JComboBox comboBox3;
-    private JPanel WriteForm;
+    private JPanel panel2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
